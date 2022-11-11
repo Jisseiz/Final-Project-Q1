15aed001601a470ec03792c8530b49cfeeca8807
Jissei <Falcon1041998@gmail.com>
2022-11-11 13:32:28 +0300

    add The code

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <=3 символам
+
+Console.WriteLine("Введите массив строк через пробел");
+string input = Console.ReadLine();
+var arr = input.Split(' ');
+
+var result = new string[arr.Length];
+var realSize = 0;
+foreach (var value in arr)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
